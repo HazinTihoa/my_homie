9b1d2b3b65dcda9097f9dc204aae902dbb06d487
HazinTihoa <tzh@tzh-rog.localdomain>
2025-06-10 20:22:33 +0800

    support paralelized data collection

--- HomieRL/legged_gym/legged_gym/scripts/play_data_collect.py
+++ HomieRL/legged_gym/legged_gym/scripts/play_data_collect.py
@@ -41,31 +41,32 @@ def draw_target_cross(env, viewer, target_pos):
 # IK func
 def solve_right_arm_ik_jacobian(
     env,
-    env_handle,
-    actor_handle,
+    env_idx,
+    actor_handle,#single
     wrist_body_index,
     arm_joint_indices,
     target_pos,
     q_init,
 ):
 
-    device = env.device  # e.g. "cuda:0"
+    device = env.device  
     rb_states_np = env.gym.get_actor_rigid_body_states(
-        env_handle, actor_handle, gymapi.STATE_ALL
+         env.envs[env_idx], actor_handle, gymapi.STATE_ALL
     )
     pos_w = rb_states_np[wrist_body_index][0][0]  # (px, py, pz)
     cur_wrist_pos = np.array([pos_w[0], pos_w[1], pos_w[2]], dtype=np.float64)
-    pos_err = target_pos - cur_wrist_pos  # numpy (3,)
-    err_norm = np.linalg.norm(pos_err)
+    pos_err = target_pos - cur_wrist_pos  # numpy (3,) current - target error vector
+    err_norm = np.linalg.norm(pos_err) # error norm
+
+
     actor_jacobian = env.gym.acquire_jacobian_tensor(
         env.sim, env.cfg.asset.name
     )  # 拿到当前全身雅可比，并提取末端对机械臂关节的 6×7 子雅可比
     env.gym.refresh_jacobian_tensors(env.sim)
     whole_jac = gymtorch.wrap_tensor(actor_jacobian)
-    j_eef = whole_jac[0, wrist_body_index, :6, arm_joint_indices]  # [6,7]，Tensorprin
-    dpos = torch.zeros(
-        6, device=device
-    )  # 构建 dpose = [pos_err; zeros(3)]，并转成 Tensor (6,1)
+    j_eef = whole_jac[env_idx, wrist_body_index, :6, arm_joint_indices]  # [6,7]，Tensorprin
+
+    dpos = torch.zeros(6, device=device)  # 构建 dpose = [pos_err; zeros(3)]，并转成 Tensor (6,1)
     dpos[0:3] = torch.from_numpy(pos_err).to(device).float()
     dpose = dpos.unsqueeze(-1)  # [6,1]     # orientation 误差置 0（不关心姿态）
 
@@ -137,10 +138,10 @@ def play(args, x_vel=0.0, y_vel=0.0, yaw_vel=0.0, height=0.74):
     # env.reset_idx(torch.arange(env.num_envs).to("cuda:0"))
     cpu_inds = torch.arange(env.num_envs, dtype=torch.int32)
     env.reset_idx(cpu_inds)
-    vx = vy = yaw = 0.0
+
 
     arrive_tollerance = 0.3  # pos tollerance
-    heading_thresh = 0.1  # yaw tollerance
+    heading_thresh = 0.32  # yaw 30 degree tollerance
     max_speed = 1  # max linear speed
     arm_end_target_err = 0.1
 
@@ -148,24 +149,23 @@ def play(args, x_vel=0.0, y_vel=0.0, yaw_vel=0.0, height=0.74):
     env.gym.clear_lines(viewer)
     B      = env.num_envs
 
-    root = env.root_states          # shape = (B, 13)，B = 并行环境数
-    pos_xyz     = root[:, 0:3]      # (B, 3)  所有机器人的 (x,y,z)
     torch.manual_seed(42)  # 这里设置种子为42
     target_pos = torch.empty(B, 3)
+
+    root = env.root_states          # shape = (B, 13)，B = 并行环境数
+    pos_xyz     = root[:, 0:3].clone()      # (B, 3)  所有机器人的 (x,y,z)
     target_pos[:, :2] = torch.rand(B, 2) * 3 - 1.5# x,y
-    
     #每个机器人中心作为目标范围的中心
     target_pos[:, 0] += pos_xyz[:, 0]  # x
     target_pos[:, 1] += pos_xyz[:, 1]  # y
-
     target_pos[:, 2] = torch.rand(B) + 0.3  # height
 
-    q_init_7dof = np.zeros(7)
     arm_joint_indices = torch.arange(26, 33, dtype=torch.long, device=env.device)
 
-    sim_step = 0
     clock = pygame.time.Clock()  # Initialize the clock for FPS calculation
     # Draw a cross at each target position for every environment
+    err_norms = torch.zeros(B, device=env.device, dtype=torch.float32)
+    need_reset = torch.zeros(B, device=env.device, dtype=torch.bool)
     for i in range(B):
         draw_target_cross(env, viewer, target_pos[i,:])
 
@@ -177,14 +177,6 @@ def play(args, x_vel=0.0, y_vel=0.0, yaw_vel=0.0, height=0.74):
         shoulder_state = rb_states[rb_shoulder_index]
         pos_w = shoulder_state[0][0]
         rot_w = shoulder_state[0][1]
-
-        dof_states = gym.get_actor_dof_states(
-            env_handle, actor_handle, gymapi.STATE_POS
-        )
-        all_pos = dof_states["pos"]
-        right_arm_pos = all_pos[20:27]
-        q_init_7dof = right_arm_pos
-
 
 
 
@@ -215,26 +207,63 @@ def play(args, x_vel=0.0, y_vel=0.0, yaw_vel=0.0, height=0.74):
 
         device = env.device
         dist_ratio = dist / (dist + 1.0)
+        vx_nominal = dist_ratio * max_speed                    # (B,)
+        yaw      = torch.clamp(dtheta * 3.0, -1.0, 1.0)
+        height_tar = torch.clamp(target_pos[:, 2], 0.3, 0.8)   # (B,)
+        idle_height = torch.full((B,), 0.75, device=device)
 
         # --- masks ---
         need_turn     = (torch.abs(dtheta) > heading_thresh)      # (B,)
         need_approach = (dist > arrive_tollerance)                       # (B,)
-
+        no_need_turn   = torch.abs(dtheta) < heading_thresh+0.2
+        no_need_approach = ~need_approach
         # --- yaw ---
-        yaw      = torch.clamp(dtheta * 2.0, -1.0, 1.0)
+
+        #if need turn：
         yaw_cmd  = torch.where(need_turn, yaw, torch.zeros_like(yaw))
+        # if need turn and need approach：
 
-        # --- vx ---
-        vx_nominal = dist_ratio * max_speed                    # (B,)
-        vx_cmd     = torch.where(need_approach, vx_nominal, torch.zeros_like(vx_nominal))
-
-        # --- height ---
-        height_tar = torch.clamp(target_pos[:, 2], 0.4, 0.9)   # (B,)
-        # 若只在“近 + 已对正”时才让高度→目标，其余保持 0.75
-        idle_height = torch.full((B,), 0.75, device=device)
-        height_cmd  = torch.where(need_turn & need_approach, idle_height, height_tar)
+        vx_cmd  = torch.where(need_approach, vx_nominal,torch.zeros_like(vx_nominal))
 
 
+        height_cmd  = torch.where(no_need_turn&no_need_approach,  height_tar, idle_height)
+        
+        pos_z     = env.root_states[:, 2]      # (B, 1)所有机器人的 (z)
+        eps          = 5e-2
+        no_need_height =torch.abs(pos_z - height_cmd) < eps
+
+        need_arm_ik=torch.where(no_need_turn&no_need_approach&no_need_height,True,False) 
+
+        ik_cal_idx  = torch.nonzero(need_arm_ik).squeeze(1)  # (k,)
+
+        if ik_cal_idx.numel():
+            n= ik_cal_idx.numel()
+            for env_idx in ik_cal_idx:
+                target_pos_ik = target_pos[env_idx, :].cpu().numpy()  # 之后转为gpu计算
+
+                dof_states = gym.get_actor_dof_states(env.envs[0], env.actor_handles[0], gymapi.STATE_POS)
+                right_arm_pos = dof_states["pos"][20:27]
+                q_init_7dof = right_arm_pos #current arm joint position
+
+                q_new, err_norm = solve_right_arm_ik_jacobian(
+                        env,
+                        env_idx,
+                        env.actor_handles[env_idx],
+                        rb_wrist_index,
+                        arm_joint_indices,
+                        target_pos_ik,
+                        q_init_7dof,
+                    )  #  arm joint get
+                right_arm_joint[env_idx,:] = torch.tensor(q_new, dtype=torch.float32) * 4
+                err_norms[env_idx]= err_norm
+                if err_norms[env_idx] < arm_end_target_err:
+                    need_reset[env_idx] = True
+        else:
+            err_norms[:] = float('inf')
+
+    
+
+        
         # if abs(dtheta) > heading_thresh:
         #     yaw_cmd = max(-1.0, min(1.0, dtheta * 2))
         #     if dist > arrive_tollerance:
@@ -265,14 +294,13 @@ def play(args, x_vel=0.0, y_vel=0.0, yaw_vel=0.0, height=0.74):
         #     else:
         #         yaw_cmd = max(-1.0, min(1.0, dtheta * 2))
 
-        right_arm_joint = torch.zeros(B,7,dtype=torch.float32)
-        vx = vx_cmd
-        yaw = yaw_cmd
+        # right_arm_joint = torch.zeros(B,7,dtype=torch.float32)
 
-        env.commands[:, 0] = torch.tensor(vx, dtype=torch.float32)
+
+        env.commands[:, 0] = vx_cmd
         env.commands[:, 1] = torch.zeros(B, dtype=torch.float32)
-        env.commands[:, 2] = torch.tensor(yaw, dtype=torch.float32)
-        env.commands[:, 4] = torch.tensor(height_cmd, dtype=torch.float32)  # height
+        env.commands[:, 2] = yaw_cmd
+        env.commands[:, 4] = height_cmd  # height
 
         actions = policy(obs.detach())
 
@@ -283,52 +311,43 @@ def play(args, x_vel=0.0, y_vel=0.0, yaw_vel=0.0, height=0.74):
 
         obs, reward, _, done, *_ = env.step(actions.detach())  # reset中也会调用
 
-        arrived_mask = (need_turn & need_approach)
-        need_reset = arrived_mask | done          # (B,) bool
+
+
         reset_ids  = torch.nonzero(need_reset).squeeze(1)  # (k,)
 
         if reset_ids.numel():
+            print("resetting====================================:")
             # env.reset_idx(reset_ids.cpu())        # 物理重置
-
-            env.gym.clear_lines(viewer)
+            n= reset_ids.numel()
             right_arm_joint[reset_ids,:] = torch.zeros(7, dtype=torch.float32)
             height_cmd[reset_ids]     = 0.75
 
-            # ④ 给这些 env 随机新目标
-            n = reset_ids.numel()
-            target_pos[reset_ids, :2] = torch.rand(n, 2, device=device) * 3 - 1.5
-            target_pos[reset_ids,  2] = torch.rand(n,   device=device)   + 0.3
+            target_pos[reset_ids, 2] = torch.rand(n, device=device)+0.3  # height
+            target_pos[reset_ids, :2] = torch.rand(n, 2, device=device) * 3 - 1.5# x,y
+            target_pos[reset_ids, 0] += pos_xyz[reset_ids, 0]  # x
+            target_pos[reset_ids, 1] += pos_xyz[reset_ids, 1]  # y
 
-            arrived_mask[reset_ids] = False    # 重置后不再满足“到达”条件   
-                    
-            # Draw a cross at each target position for every environment
+            need_reset[reset_ids] = False    # 重置后不再满足“到达”条件   
+            env.gym.clear_lines(viewer)
             for i in range(B):
                 draw_target_cross(env, viewer, target_pos[i,:])
-
-
-        # if done or reset:
-        #     env.gym.clear_lines(viewer)
-        #     right_arm_joint = torch.zeros(7, dtype=torch.float32)  # 重置右臂关节角度
-        #     height_cmd = 0.75  # reset height
-
-        #     target_pos[:, :2] = torch.rand(B, 2) * 3 - 1.5  # x,y
-        #     target_pos[:, 2] = torch.rand(B) + 0.3  # height
-
+                
 
         clock.tick(60)
         # ------- 数据采样与打印（插在 env.step() 之后）---------------------------------------------------------------------------------------------
         wall_time = time.time()  # Unix 时间戳
         sim_time = env.gym.get_sim_time(env.sim)  # 仿真时间
         fps_val = clock.get_fps()  # 实时 FPS
+        # print("fps:",fps_val)
 
         # base（机体）13 维：pos(3) + quat(4) + lin_vel(3) + ang_vel(3)
         # 全批量张量保留在 GPU（建议）
-        root = env.root_states          # shape = (B, 13)，B = 并行环境数
+        # root = env.root_states          # shape = (B, 13)，B = 并行环境数
 
-        pos_xyz     = root[:, 0:3]      # (B, 3)  所有机器人的 (x,y,z)
-        quat_xyzw   = root[:, 3:7]      # (B, 4)  (qx,qy,qz,qw)
-        lin_vel_xyz = root[:, 7:10]     # (B, 3)  线速度
-        ang_vel_xyz = root[:, 10:13]    # (B, 3)  角速度
+        # pos_xyz     = root[:, 0:3]      # (B, 3)  所有机器人的 (x,y,z)
+        # quat_xyzw   = root[:, 3:7]      # (B, 4)  (qx,qy,qz,qw)
+        # lin_vel_xyz = root[:, 7:10]     # (B, 3)  线速度
+        # ang_vel_xyz = root[:, 10:13]    # (B, 3)  角速度
 
 
         # 关节 v w
